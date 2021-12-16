5f252b3de6a8e0a4b39fe625302f5162c1e4043d
anderson.indiano <indianobox2@gmail.com>
2021-12-16 19:00:10 -0300

    Criado conexão do banco de dados H2

---
+++ src/main/java/com/anderson/indiano/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.anderson.indiano.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anderson.indiano.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Informática");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(categoria1);
+		listaCategoria.add(categoria2);
+		
+		return listaCategoria;
+	}
+}
